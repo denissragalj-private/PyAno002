f77bf94f98082e002630a9d66ae8a9813de5fd19
denissragalj-private <denis.sragalj@gmail.com>
2025-12-02 19:02:55 +0100

    aaa

--- pianos/models/pianos.py
+++ pianos/models/pianos.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from pianos.models.piano_category import PianoCategory
+
 
 class Piano(models.Model):
     model = models.CharField(max_length=150,
@@ -10,3 +12,10 @@ class Piano(models.Model):
                                    null=True,
                                    blank=True,
                                    help_text='Opis klavira')
+    category = models.ForeignKey (PianoCategory,
+                                  null=True,
+                                  default=0,
+                                  on_delete=models.SET_DEFAULT)
+
+    def __str__(self):
+        return f'{self.model}  ({self.category.name})'
