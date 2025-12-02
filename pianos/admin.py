fe45957e84b4672fedab16f7163dd0bd8bce2149
denissragalj-private <denis.sragalj@gmail.com>
2025-12-02 20:56:54 +0100

    alll

--- pianos/admin.py
+++ pianos/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
 from .models.pianos import Piano, PianoCategory, PianoType
 
-admin.site.register
+# Register your models here.
+admin.site.register([Piano, PianoCategory, PianoType])
