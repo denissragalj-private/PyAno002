c14ac1d7273fd242c7092c20cfe57d875beb93f7
denissragalj-private <denis.sragalj@gmail.com>
2025-12-16 21:09:57 +0100

    aaaaa

--- chords/urls.py
+++ chords/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 
 from . import views
 
-
 urlpatterns = [
     # path('', views.list, name='chords_list'),
     path('', views.ChordListView.as_view(), name='chords_list'),
@@ -11,5 +10,5 @@ urlpatterns = [
     # path('/create', views.create, name='chords_create')
     path('/create', views.ChordCreateView.as_view(), name='chords_create'),
     path('/update/<int:pk>', views.ChordUpdateView.as_view(), name='chords_update'),
-    path('/delete/<int:pk>', views.ChordDeleteView.as_view(), name='chords_delate')
+    path('/delete/<int:pk>', views.ChordDeleteView.as_view(), name='chords_delete')
 ]
